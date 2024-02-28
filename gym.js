503631389c532461c9b89913c6f82e16c79cfcf3
yuvraj <yuvrajsinghchouhan668@gmail.com>
2024-02-28 21:16:14 +0530

    initail commit

---
+++ gym.js
@@ -0,0 +1,25 @@
+
+let hbox = document.getElementById("box");
+let hbtn = document.querySelector(".Jbtn");
+
+hbtn.addEventListener("click", () => {
+   if(hbox.style.display == "block"){
+     hbox.style.display = "none";
+   }
+   else{
+     hbox.style.display = "block";
+   }
+ 
+})
+
+let bbox = document.getElementById("box");
+let bbtn = document.querySelector(".Jbtn2");
+
+bbtn.addEventListener("click",()=>{
+  if(bbox)
+} ) 
+
+
+
+
+
